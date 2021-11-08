413426a62b311fd8831aab965bc2627733d228da
Murodane <88901504+Murodane@users.noreply.github.com>
2021-11-08 17:39:22 -0500

    Spinner Unidad de Medida

--- app/src/main/java/com/example/appciclo4/MainActivity.java
+++ app/src/main/java/com/example/appciclo4/MainActivity.java
@@ -5,17 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.icu.text.DecimalFormat;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    protected ArrayList UnidadMedidaPeso = new ArrayList();
+    protected ArrayList UnidadMedidaTalla = new ArrayList();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UnidadMedidaPeso.add("Kg");
+        UnidadMedidaPeso.add("Lb");
+
+        UnidadMedidaTalla.add("m");
+        UnidadMedidaTalla.add("cm");
+
+        ArrayAdapter umPeso= new ArrayAdapter(MainActivity.this, android.R.layout.simple_spinner_dropdown_item,UnidadMedidaPeso);
+        ArrayAdapter umTalla= new ArrayAdapter(MainActivity.this, android.R.layout.simple_spinner_dropdown_item,UnidadMedidaTalla);
+
+        Spinner Sp_Peso =(Spinner) findViewById(R.id.sp_Peso);
+        Sp_Peso.setAdapter(umPeso);
+        Spinner Sp_Talla =(Spinner) findViewById(R.id.sp_Talla);
+        Sp_Talla.setAdapter(umTalla);
 
         EditText Peso =(EditText) findViewById(R.id.TxtPeso);
         EditText Talla =(EditText) findViewById(R.id.TxtTalla);
@@ -29,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
                 double peso_local=0;
                 double talla_local=0;
                 double resultado=0;
-
 
                 if(Peso.getText().toString().isEmpty()){
                     Peso.setError("Debe Ingresar su Peso.");
@@ -53,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
                     DecimalFormat df = new DecimalFormat("#.#");
                     //Result_IMC.append(String.valueOf(df.format(resultado)));
                     Result_IMC.setText(String.valueOf(df.format(resultado)));
+                    //Result_IMC.setText(String.valueOf(Sp_Peso.getSelectedItem().toString()));
                 }
 
             }
